4fc6e14d0ab558ba0bf359a0e2e4b2d0a757d242
Andrea Alarcón <andreaalarcon5299@gmail.com>
2022-11-02 15:58:13 +0100

    modelos creados

---
+++ models/Animal.model.js
@@ -0,0 +1,90 @@
+const { Schema, model } = require("mongoose");
+
+const animalSchema = new Schema(
+  {
+    userInfo: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        require: true
+    },
+    imgAnimal: {
+        type: String,
+            require: true
+        },
+        description: {
+            type: String,
+            require: true
+        },
+        animalName: {
+            type: String,
+            require: true
+        },
+        gender: {
+            type: String,
+            enum: ["Macho", "Hembra"],
+            require: true
+        },
+        birthday: {
+            type: String,
+            require: true
+        },
+        animalType: {
+            type: String,
+            enum: ["Perro", "Gato", "Exótico"],
+            require: true
+        },
+        animalBreed: {
+            type: Boolean
+            // raza del animal
+        },
+        weight: {
+            type: Number,
+            require: true
+        },
+        age: {
+            type: String,
+            enum: ["Cachorro", "Joven", "Adulto", "Anciano"],
+            require: true
+
+        },
+        castrated: {
+            type: Boolean,
+            require: true
+        },
+        vaccines: {
+            type: Boolean,
+            require: true
+        },
+        size: {
+            type: String,
+            enum: ["Pequeño", "Mediano", "Grande"],
+            require: true
+        },
+        lifestyle: {
+     type: String,
+            enum: ["Activo", "Muy activo", "Tranquilo", "Muy tranquilo"],
+            require: true
+        },
+        illness: {
+            // Enfermedades
+            type: Boolean,
+            require: true
+        },
+        microchip: {
+            type: Boolean,
+            require: true
+        },
+        adopted: {
+            type: Boolean,
+            default: false
+        }
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Animal = model("Animal", animalSchema);
+
+module.exports = Animal;
--- models/User.model.js
+++ models/User.model.js
@@ -3,21 +3,31 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    username: {
+      type: String,
+      required: [true, "Name is required."],
+      trim: true,
+    },
     email: {
       type: String,
       required: [true, "Email is required."],
       unique: true,
       lowercase: true,
       trim: true,
+      match: [/^\S+@\S+.\S+$/, "Please use a valid email address."]
     },
     password: {
       type: String,
       required: [true, "Password is required."],
     },
-    name: {
+    imgUser: {
       type: String,
-      required: [true, "Name is required."],
+      default: "../public/defaultimg.png"
     },
+    admin: {
+      type: Boolean,
+      default: false
+    }
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
