d73689a28595678e931cd5542871285160e9a827
Nahuel Cimini <nahuel.cimini@gmail.com>
2022-11-03 15:16:05 +0100

    cloudinary and multer

--- models/Animal.model.js
+++ models/Animal.model.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const animalSchema = new Schema(
   {
-    userInfo: {
+    creator: {
         type: Schema.Types.ObjectId,
         ref: "User",
         require: true
