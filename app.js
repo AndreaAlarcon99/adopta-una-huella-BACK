fc1b01f34fabed5c72bc8e9c1026862bdb044a34
Andrea Alarcón <andreaalarcon5299@gmail.com>
2022-11-03 20:21:00 +0100

    funciona localhost

--- app.js
+++ app.js
@@ -5,18 +5,14 @@ require("dotenv").config();
 // ℹ️ Connects to the database
 require("./db");
 
-const { isAuthenticated } = require('./middleware/jwt.middleware')
+const { isAuthenticated } = require("./middleware/jwt.middleware");
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
 const app = express();
 require("./config")(app);
 
-
 // 👇 Start handling routes here
-const indexRoutes = require("./routes/index.routes");
-app.use("/", indexRoutes);
-
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/", authRoutes);
@@ -26,7 +22,6 @@ app.use("/", isAuthenticated, animalRoutes);
 
 const userRoutes = require("./routes/user.routes");
 app.use("/", isAuthenticated, userRoutes);
-
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
